1c2792a30d845a0dab4efb130bb71599d2b694a5
Bartosz Nocun <bartosznocun1@gmail.com>
2021-02-03 19:58:49 +0100

    Add footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const navItems = document.querySelector('.nav__items');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
     navItems.classList.toggle('nav__items--active');
@@ -7,3 +8,10 @@ const handleNav = () => {
 }
 
 burgerBtn.addEventListener('click', handleNav);
+navItems.addEventListener('click', handleNav);
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+handleCurrentYear();
