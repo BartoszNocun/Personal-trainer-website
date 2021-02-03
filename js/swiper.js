627b876a551fa7455bdd44e9aee131a7c5dde92e
Bartosz Nocun <bartosznocun1@gmail.com>
2021-02-03 14:59:26 +0100

    Add price-list section

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
     loop: true,
 
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
